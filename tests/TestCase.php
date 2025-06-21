f9d83cabb4d3a672e062cdf207175469c39690ed
felac <cnastares@gmail.com>
2025-06-21 01:43:21 -0500

    Fix constants redefinition and test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $installed = storage_path('installed');
+        if (! file_exists($installed)) {
+            file_put_contents($installed, 'installed');
+        }
+    }
 }
